b6bde6620b4a1fbbcd7214cf435a97c26f915e9a
HQ106347 <jiri@solar.dk>
2022-05-24 10:58:24 +0200

    Added Lua bindings

--- main.cpp
+++ main.cpp
@@ -1,74 +1,180 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
 #include "foundation/lua.h"
 
-const std::string lua_static_script = R"(
+const std::string global_clua_name = "clua";
 
+namespace utils {
+    std::vector<std::string> string_explode(std::string const& source, char const delimiter) {
+        std::vector<std::string> parts;
+        std::istringstream iss(source);
+
+        for (std::string token; std::getline(iss, token, delimiter); ) {
+            parts.push_back(std::move(token));
+        }
+
+        return parts;
+    }
+}
+const std::string lua_script = R"(
+    print(clua.engine.version)
+
+    local version = clua.engine.getVersion();
+    print(version)
 )";
+void dumpLua(lua_State* lua_state) {
+    int top = lua_gettop(lua_state);
+    std::cout << "------------------- Starting Lua stack dump -------------------\n";
+    if (top == 0) {
+        std::cout << "Stack is empty\n";
+        return;
+    }
+    for (int i = 1; i <= top; i++) {
+        printf("%d\t%s\t", i, luaL_typename(lua_state,i));
+        switch (lua_type(lua_state, i)) {
+            case LUA_TNUMBER:
+                printf("%g\n",lua_tonumber(lua_state,i));
+                break;
+            case LUA_TSTRING:
+                printf("%s\n",lua_tostring(lua_state,i));
+                break;
+            case LUA_TBOOLEAN:
+                printf("%s\n", (lua_toboolean(lua_state, i) ? "true" : "false"));
+                break;
+            case LUA_TNIL:
+                printf("%s\n", "nil");
+                break;
+            default:
+                printf("%p\n",lua_topointer(lua_state,i));
+                break;
+        }
+    }
+    std::cout << "------------------ Lua dump finished -------------------\n\n\n";
+}
+void withLua(lua_State* lua, const char* chunk, int nargs = 0, int nresults = 0) {
+    (void) luaL_loadstring(lua, chunk);
+    lua_insert(lua, -nargs - 1);
+    (void)lua_call(lua, nargs, nresults);
+}
+/**
+ * Adds value that's currently on top of Lua stack to Lua Library API
+ * @param lua_state
+ * @param name
+ */
+void addToLuaApi(lua_State* lua_state, const char* name) {
 
-struct TestObject {
-    const char* name;
-    float value;
-    bool global;
-};
+    // this is also where the value that will be added to stack lives
+    const int stack_offset = lua_gettop(lua_state);
+    if (stack_offset == 0) {
+        throw std::runtime_error("[addToLuaApi] Cannot add key to Lua API because current stack size is 0. Please push value you wish to be added to Lua stack before calling this method");
+    }
 
-class Application {
-private:
-    static Lua* m_luaModule;
-public:
-    static void InitializeModules() {
-        // Could be smart pointer
-        m_luaModule = new Lua();
+    const auto name_exploded = utils::string_explode(name, '.');
+
+    // top-level
+    lua_getglobal(lua_state, global_clua_name.c_str());
+
+    // create new global Clua API if it does not exist
+    if (lua_isnoneornil(lua_state, stack_offset + 1)) {
+        lua_pop(lua_state, 1);
+
+        lua_newtable(lua_state);
+        lua_setglobal(lua_state, global_clua_name.c_str());
+
+        lua_getglobal(lua_state, global_clua_name.c_str());
+    } else {
+        // Otherwise, verify that it is a table
+        if (!lua_istable(lua_state, stack_offset + 1)) {
+            lua_pop(lua_state, 1);
+
+            std::string error ("Tried to create '");
+            error.append(global_clua_name);
+            error.append("' but it already exists, and is not a table");
+
+            throw std::runtime_error(error.c_str());
+        }
     }
-    static void TerminateModules() {
-        delete Application::m_luaModule;
+
+    // add nested subtables
+    int local_stack_offset = 0;
+    for (auto i = name_exploded.begin(); i != name_exploded.end() - 1; ++i) {
+        auto field_name = *i;
+        auto field = lua_getfield(lua_state, stack_offset + local_stack_offset + 1, field_name.c_str());
+
+        if (lua_isnoneornil(lua_state, local_stack_offset + stack_offset + 2)) {
+            lua_pop(lua_state, 1);
+
+            lua_newtable(lua_state);
+            lua_setfield(lua_state, local_stack_offset + stack_offset + 1, field_name.c_str());
+
+            field = lua_getfield(lua_state, stack_offset + local_stack_offset + 1, field_name.c_str());
+        } else {
+            if (!lua_istable(lua_state, local_stack_offset + stack_offset + 2)) {
+                lua_pop(lua_state, 1);
+
+                std::string error ("Tried to create member'");
+                error.append(field_name);
+                error.append("' but it already exists, and is not a table");
+
+                throw std::runtime_error(error.c_str());
+            }
+        }
+        local_stack_offset++;
     }
-    static Lua* GetLuaModule() {
-        return Application::m_luaModule;
-    }
-};
+
+    // move value to the top of the stack
+    lua_rotate(lua_state, stack_offset, local_stack_offset + 1);
+
+    // set field
+    lua_setfield(lua_state, stack_offset + local_stack_offset, name_exploded[name_exploded.size() - 1].c_str());
+
+    // cleanup stack - dont forget to cleanup toplevel global table, hence +1
+    lua_pop(lua_state, local_stack_offset + 1);
+
+    /**
+     * Lua Types
+     * LUA_TNIL (0)
+     * LUA_TNUMBER
+     * LUA_TBOOLEAN
+     * LUA_TSTRING
+     * LUA_TTABLE
+     * LUA_TFUNCTION
+     * LUA_TUSERDATA
+     * LUA_TTHREAD
+     * LUA_TLIGHTUSERDATA
+     */
+
+}
 
 int main() {
 
-    Application::InitializeModules();
+    /** Load LUA */
+    lua_State* lua_state = luaL_newstate();
 
-    const auto lua = Application::GetLuaModule();
+    lua_gc(lua_state, LUA_GCSTOP, 0);
+    luaL_openlibs(lua_state);
+    lua_gc(lua_state, LUA_GCRESTART, 0);
 
-    std::vector<TestObject> tests {
-        { "localTest", 1.0f, false },
-        { "test", 2.0f, true },
-        { "localTest.nested1", 3.0f, false },
-        { "test.nested1", 4.0f, true },
-        { "localTest.nested2.nested1", 5.0f, false },
-        { "test.nested2.nested1", 6.0f, true },
-        { "localTest.nested2.nested2", 7.0f, false },
-        { "test.nested2.nested2", 8.0f, true }
-    };
+    /** Add bindings */
+    lua_pushnumber(lua_state, 0.1);
+    addToLuaApi(lua_state, "engine.version");
 
+    lua_pushcfunction(lua_state, [](lua_State* lua){
+        lua_pushnumber(lua, 0.1);
+        return 1;
+    });
+    addToLuaApi(lua_state, "engine.getVersion");
 
-    /**
-     * Tests:
-     * localTest:1
-     * clua.test:2
-     * localTest.nested1:3
-     * clua.test.nested1:4
-     * localTest.nested2.nested1:5
-     * clua.test.nested2.nested1:6
-     * localTest.nested2.nested2:7
-     * clua.test.nested2.nested2:8
-     */
+    /** Run */
+    withLua(lua_state, lua_script.c_str(), 0);
 
-    for (const auto i : tests) {
-        lua->SetFloat(i.name, i.value, i.global);
-    }
-
-    lua->Run(lua_static_script.c_str());
-
-    Application::TerminateModules();
+    /** Close LUA */
+    lua_close(lua_state);
 
     return 0;
 }
--- source/foundation/lua.cpp
+++ source/foundation/lua.cpp
@@ -3,7 +3,6 @@
 
 Lua::Lua(): m_state(luaL_newstate()) {
     LoadLibraries();
-    SetGlobals();
 }
 Lua::~Lua() {
     lua_close(m_state);
@@ -11,27 +10,8 @@ Lua::~Lua() {
 lua_State* Lua::GetState() const {
     return m_state;
 }
-void Lua::Run(const char* source) {
-    (void)luaL_loadstring(m_state, source);
-    (void)lua_call(m_state, 0, 0);
-}
-void Lua::SetGlobals() {
-    lua_newtable(m_state);
-
-    lua_pushstring(m_state, "hello");
-    lua_pushcfunction(m_state, [](lua_State* lua){
-        std::cout << "hello from Clua engine!\n";
-        return 0;
-    });
-
-    lua_settable(m_state, -3);
-    lua_setglobal(m_state, m_globalTableName);
-}
 void Lua::LoadLibraries() {
     lua_gc(m_state, LUA_GCSTOP, 0);
     luaL_openlibs(m_state);
     lua_gc(m_state, LUA_GCRESTART, 0);
-}
-void Lua::SetFloat(const char* name, float value, const bool globalApi) {
-    std::cout << "adding '" << (globalApi ? "clua." : "") << name << "' with value " << std::to_string(value) << "\n";
 }
